03747372e4c42ca080e11f1da61b092efe487058
Coldfirst <91018047+Coldfirst@users.noreply.github.com>
2021-09-26 16:02:37 +0300

    Update 03_task_string.py

--- Module3/practice/string/03_task_string.py
+++ Module3/practice/string/03_task_string.py
@@ -1,3 +1,8 @@
 # Подсчитать количество букв «а» во введенной строке
 
 # TODO: your code here
+
+
+text = input('Введите текст, например Рамамба Хару Мамбуру ')
+new_text = text.count('а')
+print(new_text)
