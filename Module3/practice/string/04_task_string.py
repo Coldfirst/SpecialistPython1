b75192d8b6dc6b5f8c127f3fc92f8a1586c773db
Coldfirst <91018047+Coldfirst@users.noreply.github.com>
2021-10-03 11:23:42 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -1,7 +1,23 @@
 # Дана строка текста, слова разделены пробелами, знаки препинания отсутствуют.
 # Определить в предоставленном сообщении количество слов длиной больше, чем 5.
 
+#text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
+# Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
+
+# TODO: your code here
+
+
+# Дана строка текста, слова разделены пробелами, знаки препинания отсутствуют.
+# Определить в предоставленном сообщении количество слов длиной больше, чем 5.
+
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+words = text.split (" ")
+num_long_words = 0
+for word in words:
+    if len (word) >5:
+        num_long_words += 1
+
+print(num_long_words)
