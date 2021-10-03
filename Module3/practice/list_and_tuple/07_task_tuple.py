09a573495a0138b7b7c262101e460f92ec8397df
Coldfirst <91018047+Coldfirst@users.noreply.github.com>
2021-10-03 14:08:10 +0300

    Update 07_task_tuple.py

--- Module3/practice/list_and_tuple/07_task_tuple.py
+++ Module3/practice/list_and_tuple/07_task_tuple.py
@@ -3,3 +3,12 @@
 tup = (2, 4, 6, -4, 12, 0, 5)
 
 # TODO: your code here
+
+
+# Дан кортеж заполненный целыми числами
+# Найдите самый большой элемент кортежа
+tup = (2, 4, 6, -4, 12, 0, 5)
+
+# TODO: your code here
+newtup =max(tup, key=int)
+print(newtup)
