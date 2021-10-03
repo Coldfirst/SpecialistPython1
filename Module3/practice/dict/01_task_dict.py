1f8d5489132713199973346baf7ebe2f74638a28
Coldfirst <91018047+Coldfirst@users.noreply.github.com>
2021-10-03 15:05:55 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -8,3 +8,17 @@ item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "cou
 dollar_rate = 74.12
 
 # TODO: your code here
+
+
+# Дан словарь, содержащий данные о товаре из магазина
+# "price" - цена товара в валюте "currency"
+# "count" - количествотовара в магазине
+# Считая,что курс доллара равен dollar_rate
+# Вычислите цену всех товаров с названием "name" в долларах
+
+item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "count": "10"}
+dollar_rate = 74.12
+
+# TODO: your code here
+total = float(item.get('price')) * int(item.get('count')) /dollar_rate
+print(int(total))
