5f09cd9d10686ebfb6c10c5c7029b7e1d8cefb33
Coldfirst <91018047+Coldfirst@users.noreply.github.com>
2021-10-03 12:24:08 +0300

    Update 02_task_list.py

--- Module3/practice/list_and_tuple/02_task_list.py
+++ Module3/practice/list_and_tuple/02_task_list.py
@@ -4,3 +4,17 @@
 fruits = ["яблоко", "банан", "киви", "ананас", "груша"]
 
 # TODO: your code here
+
+
+# fruits = ["яблоко", "банан", "киви", "ананас", "груша"]
+#
+# # TODO: your code here
+# a=1
+# for e in fruits:
+#         print (a,e)
+#         a+=1
+
+fruits = ["яблоко", "банан", "киви", "ананас", "груша"]
+
+for number, fruit in enumerate(fruits, 1):
+        print(number, fruit)
