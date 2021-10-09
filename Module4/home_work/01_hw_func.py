e06018ac04e72c6671e5c211b9ec2e86014004a1
Coldfirst <91018047+Coldfirst@users.noreply.github.com>
2021-10-10 00:54:47 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -11,3 +11,24 @@ def lucky_ticket(ticket_number):
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+
+# Дан шестизначный номер билета. Определить, является ли билет счастливым.
+# Решение реализовать в виде функции.
+# Билет считается счастливым, если сумма его первых и последних цифр равны.
+def lucky_ticket(ticket_number):
+    # TODO: your code here
+
+    pass
+    num=str(ticket_number)
+    sum1=int(num[0])+int(num[1])+int(num[2])
+    sum2=int(num[3])+int(num[4])+int(num[5])
+    if sum1 == sum2:
+        result="+"
+    else:
+        result="-"
+    return result
+
+# Тест
+print(lucky_ticket(123006))
+print(lucky_ticket(912321))
+print(lucky_ticket(436751))
