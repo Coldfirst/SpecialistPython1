63a9e6ed210895d354047541328d8a33076930f4
Coldfirst <91018047+Coldfirst@users.noreply.github.com>
2021-10-10 15:23:24 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -5,3 +5,13 @@
 
 def palindrome(number):
     pass
+
+
+def palindrome(number):
+    our_number = str(number)
+    return our_number == our_number[::-1]
+
+# Тестируем функцию
+print(palindrome(1234))
+print(palindrome(3443))
+print(palindrome(1))
