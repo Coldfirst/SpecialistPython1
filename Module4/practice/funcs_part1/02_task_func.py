5bd0f70fff445b7df4992f430488665e218a60e1
Coldfirst <91018047+Coldfirst@users.noreply.github.com>
2021-10-10 12:08:35 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -13,3 +13,21 @@ print(palindrome(3443))
 print(palindrome(1234541))
 print(palindrome(1234321))
 print(palindrome(77777))
+
+
+# Напишите функцию,  определяющую является ли число палиндромом.
+# Палиндром - число читающееся одинаково слева направо и справа налево.
+# Пример палиндрома: 34543
+# * попробуйте решить данную задачу, не преобразуя число к строке
+
+def palindrome(number):
+    my_number = str(number)
+    return my_number == my_number [::-1]
+
+
+# Тестируем функцию
+print(palindrome(3454))
+print(palindrome(3443))
+print(palindrome(1234541))
+print(palindrome(1234321))
+print(palindrome(77777))
