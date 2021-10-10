4754f6b60d0ab28ef4adb30f1db421b7d41ea49b
Coldfirst <91018047+Coldfirst@users.noreply.github.com>
2021-10-10 14:24:52 +0300

    Update 10_task_func.py

--- Module4/practice/funcs_part2/10_task_func.py
+++ Module4/practice/funcs_part2/10_task_func.py
@@ -9,3 +9,14 @@ def average(*args):
 print(average(3, 4, 8))
 print(average(1, 4, 5, -3, 8, 4))
 print(average(-10, 12, 6.3, -5.5, 7, 0.2))
+
+
+
+def average(*args):
+    # TODO: your code here
+    return sum (args)/len(args)
+
+print(average(1,0))
+print(average(3, 4, 8))
+print(average(1, 4, 5, -3, 8, 4))
+print(average(-10, 12, 6.3, -5.5, 7, 0.2))
